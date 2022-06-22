5520c85851e8ee6124828b807ec3fda0e4712849
Jose Pumarejo <jose.pumarejo02@gmail.com>
2022-06-21 20:14:22 -0500

    apendiendo yield para evitar la repeticion de codigo

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController as HomeController;
 use App\Http\Controllers\DashboardController as DashboardController;
+use App\Http\Controllers\ExpenseReportController as ExpenseReportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard',  [DashboardController::class, 'index']);
+
+Route::resource('/expense_reports', ExpenseReportController::class);
